ab166ed916dcac8bb34096fecb4cda8c1e1ac25d
dreamlabss <denisov1642@gmail.com>
2020-06-09 23:19:47 +0700

    homework for lesson 5

--- homework_lesson5.py
+++ homework_lesson5.py
@@ -140,3 +140,6 @@ with open('file_7.json', 'w') as write_js:
     js_str = json.dumps(profit)
     print(f'Создан файл с расширением json со следующим содержимым: \n '
           f' {js_str}')
+
+
+#dsasdfsdf
